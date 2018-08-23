932b589d451ec3ca27c1d9c297895cc0a6d4d6f2
Antonius Yonathan <atomicbomber.email@gmail.com>
2018-08-23 10:34:35 +0700

    Changed button sizes.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,14 +21,19 @@
             <hr class="mt-2 mb-2">
 
             <p class="card-text mt-5">
-                <a href="{{ route('vendor.index') }}" class="btn btn-info">
+                <a href="{{ route('vendor.index') }}" class="btn btn-info btn-lg">
                     <i class="fa fa-shopping-cart"></i>
                     Vendors
                 </a>
 
-                <a href="{{ route('item.index') }}" class="btn btn-info">
+                <a href="{{ route('item.index') }}" class="btn btn-info btn-lg">
                     <i class="fa fa-list"></i>
                     Items
+                </a>
+
+                <a href="{{ route('storage.index') }}" class="btn btn-info btn-lg">
+                    <i class="fa fa-truck"></i>
+                    Storages
                 </a>
             </p>
         </div>
