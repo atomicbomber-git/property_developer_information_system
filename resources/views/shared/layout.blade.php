0c1db3d1b1261e16837acf54c6c9f509d132c93c
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-10-09 22:08:46 +0700

    Various fixes.

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -28,7 +28,7 @@
             </div>
         </nav>
 
-        <div class="container">
+        <div class="container" id="app">
             <div class="alert alert-info">
                 <i class="fa fa-info"></i>
                 Anda Log In dengan nama {{ auth()->user()->name }} dan status {{ auth()->user()->privilege }}
